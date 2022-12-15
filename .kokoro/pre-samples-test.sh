27262f0305b33ed0ca2736506788d361e85c8849
Daniel Bankhead <danielbankhead@google.com>
2022-12-15 13:33:38 -0800

    feat(samples): Add Azure Sample (#134)

--- .kokoro/pre-samples-test.sh
+++ .kokoro/pre-samples-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2018 Google LLC
+# Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,3 +18,9 @@
 export STS_AWS_SECRET=`gcloud secrets versions access latest --project cloud-devrel-kokoro-resources --secret=nodejs-storagetransfer-aws`
 export AWS_ACCESS_KEY_ID=`S="$STS_AWS_SECRET" node -p "JSON.parse(process.env.S).AccessKeyId"`
 export AWS_SECRET_ACCESS_KEY=`S="$STS_AWS_SECRET" node -p "JSON.parse(process.env.S).SecretAccessKey"`
+
+# Import secrets for Azure integration testing.
+export STS_AZURE_SECRET=`gcloud secrets versions access latest --project cloud-devrel-kokoro-resources --secret=nodejs-storagetransfer-azure`
+export AZURE_STORAGE_ACCOUNT=`S="$STS_AZURE_SECRET" node -p "JSON.parse(process.env.S).StorageAccount"`
+export AZURE_CONNECTION_STRING=`S="$STS_AZURE_SECRET" node -p "JSON.parse(process.env.S).ConnectionString"`
+export AZURE_SAS_TOKEN=`S="$STS_AZURE_SECRET" node -p "JSON.parse(process.env.S).SAS"`
